9d190a559d08283decde14afa047beb5d09b1654
axellindsay <ajlindsay3@gmail.com>
2017-02-09 21:17:14 -0800

    Add accessor and mutator methods.

--- src/ca/bcit/comp1451/lab1a/lindsay/alex/Book.java
+++ src/ca/bcit/comp1451/lab1a/lindsay/alex/Book.java
@@ -1,5 +1,7 @@
 package ca.bcit.comp1451.lab1a.lindsay.alex;
 
+import ca.bcit.comp1451.lab1b.lindsay.alex.Person;
+
 /**
  * Created by Alex on 2/8/2017.
  *
@@ -32,5 +34,44 @@ public class Book
     {
         this.title = title;
         this.datePublished = new Date(year, month, day);
+    }
+
+    /**
+     * Accessor method for the title.
+     * @return title as a String
+     */
+    public String getTitle()
+    {
+        return this.title;
+    }
+
+    /**
+     * Accessor method for the date published.
+     * @return datePublished as a Date.
+     */
+    public Date getDatePublished()
+    {
+        return this.datePublished;
+    }
+
+    /**
+     * Mutator method for the title.
+     * @param title as a String
+     */
+    public void setTitle(String title)
+    {
+        if (title.trim().length() > 0)
+        {
+            this.title = title;
+        }
+    }
+
+    /**
+     * Mutator method for the date published.
+     * @param date as a Date
+     */
+    public void setDatePublished(Date date)
+    {
+        this.datePublished = date;
     }
 }

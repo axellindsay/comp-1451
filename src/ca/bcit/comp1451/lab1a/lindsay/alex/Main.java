627de49022fa1db638a09ef7eff76d02ad6fba7b
axellindsay <ajlindsay3@gmail.com>
2017-02-09 21:18:16 -0800

    Add the driver class.

---
+++ src/ca/bcit/comp1451/lab1a/lindsay/alex/Main.java
@@ -0,0 +1,16 @@
+package ca.bcit.comp1451.lab1a.lindsay.alex;
+
+/**
+ * Created by Alex on 2/9/2017.
+ *
+ * Driver class for our bookstore.
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        BookStore bookstore = new BookStore();
+
+        bookstore.displayBooks();
+    }
+}

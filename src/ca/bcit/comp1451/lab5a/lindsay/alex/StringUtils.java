d9702ba108761a93bba55202edd6530d0b00a36e
axellindsay <ajlindsay3@gmail.com>
2017-02-11 01:16:55 -0800

    String utils.

---
+++ src/ca/bcit/comp1451/lab5a/lindsay/alex/StringUtils.java
@@ -0,0 +1,44 @@
+package ca.bcit.comp1451.lab5a.lindsay.alex;
+
+/**
+ * Created by Alex on 2/11/2017.
+ *
+ * Class for String utils.
+ */
+public class StringUtils
+{
+    /**
+     * Check if a String is not null and has a length of greater than 0.
+     * @param s String to check.
+     * @return True if valid, else false.
+     */
+    public static boolean checkString(String s)
+    {
+        return s != null && s.trim().length() > 0;
+    }
+
+    /**
+     * Format a String to be capitalized and trimmed.
+     * @param s String to format.
+     * @return The capitalized and trimmed String.
+     */
+    public static String formatString(String s)
+    {
+        s = s.trim();
+        return s.substring(0, 1).toUpperCase() + s.substring(1).toLowerCase();
+    }
+
+    public static boolean isNumeric(String s)
+    {
+        try
+        {
+            Integer.parseInt(s);
+        }
+        catch(NumberFormatException NFE)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
